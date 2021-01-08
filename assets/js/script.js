b0100bf0410fb22a320e0c6493c051a582908e1d
nhardiansa <nhardiansa@gmail.com>
2021-01-08 22:37:31 +0800

    menambahkan animasi navbar dan menu mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,18 @@ const button = document.querySelector(".icon button")
 const arrow1 = document.querySelector(".icon button svg.arrow1")
 const arrow2 = document.querySelector(".icon button svg.arrow2")
 
+const menu = document.querySelector(".menu");
+
+const navbar = document.querySelector("nav");
 
 button.addEventListener("click", ()=>{
     arrow1.classList.toggle("up");
     arrow2.classList.toggle("down");
+    menu.classList.toggle("click");
+})
+
+window.addEventListener("scroll", () => {
+    
+    navbar.classList.toggle("sticky",window.scrollY > 0);
+
 })
